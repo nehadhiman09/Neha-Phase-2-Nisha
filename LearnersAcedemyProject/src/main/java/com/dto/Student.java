822747044f9f742aafd1eae496f0c54e9471cf60
nehadhiman09 <nehadhiman392@gmail.com>
2021-04-19 20:29:19 +0530

    Hibernate

---
+++ LearnersAcedemyProject/src/main/java/com/dto/Student.java
@@ -0,0 +1,45 @@
+package com.dto;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="student_info")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+
+}
